834448664302f0e132ec11f2ffffcfc042255251
fallougaye19 <125678450+fallougaye19@users.noreply.github.com>
2025-11-18 19:23:54 +0000

    Fix syntax error in app.run call

--- app.py
+++ app.py
@@ -201,4 +201,4 @@ if __name__ == '__main__':
 
     # Lancer l'application
     port = int(os.environ.get("PORT", 5000))
-    app.run(debug=False, host='0.0.0.0', port=port
+    app.run(debug=False, host='0.0.0.0', port=port)
