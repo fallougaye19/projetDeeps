1401b74d1e081827beca61468f1d0ee6bd9c6fcc
fallougaye19 <125678450+fallougaye19@users.noreply.github.com>
2025-11-18 19:13:45 +0000

    Change app port to use environment variable

--- app.py
+++ app.py
@@ -200,4 +200,5 @@ if __name__ == '__main__':
     print("="*70 + "\n")
 
     # Lancer l'application
-    app.run(debug=True, host='0.0.0.0', port=5001)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host='0.0.0.0', port=port
